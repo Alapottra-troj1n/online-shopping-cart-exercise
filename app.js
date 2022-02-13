8457a60243c8be9e5fe5cce09dbae384d23812d8
Alapottra <alapottratrojon@gmail.com>
2022-02-13 11:39:30 +0600

    added total price update functionality

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const cartContainer = document.querySelector(".cart");
 
-function updateCount(isPlus, productId) {
-  const productCount = document.querySelector(productId + "-count");
+function updateCount(isPlus, product, productPrice) {
+  const productCount = document.querySelector(product + "-product-count");
   productCountValue = parseInt(productCount.value);
 
   if (isPlus == true) {
     productCountValue++;
-  } else if (isPlus == false) {
+  } else if (productCountValue > 0) {
     productCountValue--;
   }
+  //this checks if product value is greater than 0 if it is it and user clicked minus which sends false and
+  // it goes to else if statement and decrements by 1 and if the value is 0 which is not greater than 0 and user clicked minus which sends a false arguement it doees not passes if statement nor //else statement it goes down and executes other codes 
   productCount.value = productCountValue;
+
+
+  const productTotalprice = document.querySelector(product+ '-total-price');
+
+  let totalProductPrice = productCountValue * productPrice;
+
+  productTotalprice.innerText = totalProductPrice;
+
+  
 }
 
 
@@ -17,17 +28,13 @@ function updateCount(isPlus, productId) {
 
 
 cartContainer.addEventListener("click", function (e) {
-  const caseProduct = ".case-product";
   if (e.target.classList.contains("case-plus-btn")) {
-    updateCount(true, caseProduct);
+    updateCount(true,'.case', 59);
+    
    
   } else if (e.target.classList.contains("case-minus-btn")) {
-    updateCount(false, caseProduct);
-    const productCount = document.querySelector('.case-product-count');
-    if(productCount.value == 0) {
 
-        productCount.value = 1;
-
-    }
+    updateCount(false, '.case', 59);
+    
   }
 });
